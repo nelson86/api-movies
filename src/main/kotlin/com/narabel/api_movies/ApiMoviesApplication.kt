56da097bcdb8371910672c3377684506ee03a0be
Nelson Arabel <nelson.arabel@gmail.com>
2022-02-24 10:08:01 -0300

    Iniciando...

---
+++ src/main/kotlin/com/narabel/api_movies/ApiMoviesApplication.kt
@@ -0,0 +1,11 @@
+package com.narabel.api_movies
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiMoviesApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiMoviesApplication>(*args)
+}
